907347b3818861a97d6a5322fbd6d8b066573f0b
Chris Burkart <cburkart@gmail.com>
2015-03-01 17:07:53 -0600

    All exercises for Variables chapter complete.

---
+++ Variables/name.rb
@@ -0,0 +1,11 @@
+puts "Please enter your first name: "
+firstname = gets.chomp
+
+puts "Please enter your last name: "
+lastname = gets.chomp
+
+puts "Hello, " + firstname + " " + lastname
+
+10.times do
+    puts firstname + " " + lastname
+end
